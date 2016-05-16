ed8aa82d180d39dac89067dac04ce2fdee5a7468
Chris Brocklebank <chrisb11@gmail.com>
2016-05-16 00:07:43 -0700

    It works
    app.js
    Added Actor and Player classes.
    Made Enemy a subclass of Actor.
    
    updated README.md

--- README.md
+++ README.md
@@ -1,6 +1,45 @@
 frontend-nanodegree-arcade-game
 ===============================
 
-Students should use this [rubric](https://www.udacity.com/course/viewer/#!/c-nd001/l-2696458597/m-2687128535) for self-checking their submission.
+This is my first attempt at making a game in javascript. The engine and assets were provided by [Udacity](https://www.udacity.com/) as part of the frontend-nanodegree-arcade-game project. I have only modified parts of the app.js file to provide the Actor, Enemy and, Player objects.
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+# How to play
+1. Open index.html
+2. Use arrow keys to move
+3. Keep away from the giant bugs
+4. Jump in the water
+5. Repeat...
+
+# About the code
+I added this array to make it easier to set the starting y position of enemies in rows
+```javascript
+var enemyRows = [61, 144, 226];
+```
+
+The `detectCollision` method was added to the Actor superclass in case other subclass objects needed to initiate it at any point in the future. Offsets are used to try to make the collisions appear more natural without editing the image resources. The detection algorithm is based on this example from [MDN](https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection#Axis-Aligned_Bounding_Box)
+```javascript
+Actor.prototype.detectCollision = function(otherActor) {
+  return (
+    this.x + this.xOffset < otherActor.x + otherActor.xOffset + otherActor.width &&
+    this.x + this.xOffset + this.width > otherActor.x + otherActor.xOffset &&
+    this.y + this.yOffset < otherActor.y + otherActor.yOffset + otherActor.height &&
+    this.height + this.y + this.yOffset > otherActor.y + otherActor.yOffset
+  );
+};
+```
+
+`Player.prototype.handleInput` sets movement deltas that are acted upon in `Player.prototype.move`. `move` is called during the `Player.prototype.update` which ties player movement to the game loop. Example of "Left" movement in the handler:
+```javascript
+case 'left':
+  if (this.x > 1) this.dx -= 100;
+  break;
+```
+
+# Things to do
+1. Make the player avatar interchangeable
+2. Add gems and score
+3. Change number, direction, and speed range of enemies for subsequent levels
+4. Sound would be neat
+
+# License
+This software is released under [The Unlicense](http://unlicense.org/).
--- js/app.js
+++ js/app.js
@@ -1,36 +1,149 @@
+// y values for placing enemies
+var enemyRows = [61, 144, 226];
+
+// superclass for Enemy and Player
+var Actor = function(h, w, xa, ya, img) {
+  // for placement/movement of actors
+  this.x = 0;
+  this.y = 0;
+
+  // define visible actor size for collision detection
+  this.height = h;
+  this.width = w;
+
+  // store position offset within actor png, also for collision detection
+  this.xOffset = xa;
+  this.yOffset = ya;
+
+  // store image path
+  this.sprite = img;
+};
+
+// Draw the actor on the screen, required method for game
+Actor.prototype.render  = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+// detect collision with another actor
+Actor.prototype.detectCollision = function(otherActor) {
+  return (
+    this.x + this.xOffset < otherActor.x + otherActor.xOffset + otherActor.width &&
+    this.x + this.xOffset + this.width > otherActor.x + otherActor.xOffset &&
+    this.y + this.yOffset < otherActor.y + otherActor.yOffset + otherActor.height &&
+    this.height + this.y + this.yOffset > otherActor.y + otherActor.yOffset
+  );
+};
+
+// helper function for generating random ints within a given range
+var randomRange = function(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    Actor.call(this, 73, 101, 0, 73, 'images/enemy-bug.png');
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    // initialize position and speed
+    this.reset();
+
 };
-
+Enemy.prototype = Object.create(Actor.prototype);
+Enemy.prototype.constructor = Enemy;
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.move(dt);
+    this.handleCollision();
+    if (this.x > 505) this.reset();
 };
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+// adjsut enemy's position according to speed and dt
+Enemy.prototype.move = function(dt) {
+  this.x += (this.speed  * dt);
+};
+// reset player if collision detected
+Enemy.prototype.handleCollision = function() {
+  if (this.detectCollision(player)) {
+    //console.log({enemyX: this.x, playerX: player.x, enemyY: this.y, playerY: player.y});
+    player.reset();
+  }
+};
+// set random starting position and speed
+Enemy.prototype.reset = function() {
+  this.x = -100;
+  this.y = enemyRows[randomRange(0,2)];
+  this.speed = randomRange(100,400);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+  Actor.call(this, 84, 74, 14, 60, 'images/char-boy.png');
 
+  // delta x and y values
+  this.dx = 0;
+  this.dy = 0;
+
+  // set player starting position
+  this.reset();
+};
+Player.prototype = Object.create(Actor.prototype);
+Player.prototype.constructor = Player;
+// update player's attributes
+Player.prototype.update = function() {
+  this.move();
+  // y == -15 corresponds to the player reaching the water
+  if (this.y == -15) this.reset();
+};
+// adjust player's position based on stored input
+Player.prototype.move = function() {
+  //console.log({x: this.x, y: this.y, dx: this.dx, dy: this.dy});
+  this.x += (this.dx);
+  this.y += (this.dy);
+
+  // reset deltas
+  this.dx = 0;
+  this.dy = 0;
+};
+// set player to starting position
+Player.prototype.reset = function() {
+  this.x = 201;
+  this.y = 400;
+};
+// update deltas so movement can be executed by game loop
+Player.prototype.handleInput = function(keyCode) {
+  switch (keyCode) {
+    case 'left':
+      if (this.x > 1) this.dx -= 100;
+      break;
+    case 'right':
+      if (this.x < 401) this.dx += 100;
+      break;
+    case 'up':
+      if (this.y > -15) this.dy -= 83;
+      break;
+    case 'down':
+      if (this.y < 400) this.dy += 83;
+      break;
+    default:
+
+  }
+  //console.log({x: this.x + this.dx, y: this.y + this.dy});
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+var allEnemies = [];
+var countEnemies = 3;
+for (var i = 0; i < countEnemies; i++) {
+  allEnemies.push(new Enemy());
+}
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
